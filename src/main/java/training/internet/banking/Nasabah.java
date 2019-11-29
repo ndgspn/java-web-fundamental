d463005bf72ed3c6fffe6ae64549fd02984832d9
Nan <nandangsopyan08@gmail.com>
2019-11-29 16:32:35 +0700

    latihan

--- src/main/java/training/internet/banking/Nasabah.java
+++ src/main/java/training/internet/banking/Nasabah.java
@@ -1,37 +1,39 @@
 package training.internet.banking;
 
 public class Nasabah {
+	// public static integer jumlahNasabah = 0
+	// variable nomor, nama, email
+	// constructor Nasabah jumlahNasabah
+	// getter setter
+	// tampilkan jumlah nasabah
+	
 	public static Integer jumlahNasabah = 0;
 	private String nomor;
 	private String nama;
 	private String email;
-
+	
 	public Nasabah() {
 		jumlahNasabah = jumlahNasabah + 1;
-	}
-	
-	// getter setter
-	
-
-	public String getNama() {
-		System.out.println(nama + " adalah nasabah ke-"+jumlahNasabah);
-		return nama;
-	}
-	
-	public void setNama(String nama) {
-		this.nama = nama;
 	}
 	
 	public String getNomor() {
 		return nomor;
 	}
 	
-	public void setNomor(String nomor) {
-		this.nomor = nomor;
+	public String getNama() {
+		return nama;
 	}
 	
 	public String getEmail() {
 		return email;
+	}
+	
+	public void setNomor(String nomor) {
+		this.nomor = nomor;
+	}
+	
+	public void setNama(String nama) {
+		this.nama = nama;
 	}
 	
 	public void setEmail(String email) {
@@ -39,9 +41,6 @@ public class Nasabah {
 	}
 	
 	public static void tampilkanJumlahNasabah() {
-		System.out.println("Jumlah nasabah saat ini adalah: "+ jumlahNasabah);
-		//System.out.println("Nasabah terbaru adalah " + nama);
+		System.out.println("Jumlah Nasabah : "+jumlahNasabah);
 	}
-
-
 }
